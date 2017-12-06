23c17bc387d715af9c6fb0c432c99156de6ef233
Bogdan Let <bogdangeorgelet@gmail.com>
2017-12-06 17:36:25 +0200

    configuration finished

--- rest-service/src/main/java/com/catalogmanagement/CatalogManagementApplication.java
+++ rest-service/src/main/java/com/catalogmanagement/CatalogManagementApplication.java
@@ -1,7 +1,13 @@
 package com.catalogmanagement;
 
+import com.catalogmanagement.model.CompanyEntity;
+import com.catalogmanagement.model.EmployeeEntity;
+import com.catalogmanagement.repository.ICompanyRepository;
+import com.catalogmanagement.repository.IEmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatalogManagementApplication {
@@ -9,31 +15,32 @@ public class CatalogManagementApplication {
         SpringApplication.run(CatalogManagementApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner databaseSetup(ICompanyRepository companyRepository, IEmployeeRepository employeeRepository) {
-//        return args -> {
-//            CompanyEntity company1 = new CompanyEntity("Fortech");
-//            CompanyEntity company2 = new CompanyEntity("Endava");
-//            CompanyEntity company3 = new CompanyEntity("Pitech+");
-//            CompanyEntity company4 = new CompanyEntity("3PillarGlob");
-//            CompanyEntity company5 = new CompanyEntity("Scitec");
-//
-//            companyRepository.save(company1);
-//            companyRepository.save(company2);
-//            companyRepository.save(company3);
-//            companyRepository.save(company4);
-//            companyRepository.save(company5);
-//
-//
-//            employeeRepository.save(new EmployeeEntity("Mircea", "Pop", company1));
-//            employeeRepository.save(new EmployeeEntity("Suciu", "Andrei", company5));
-//            employeeRepository.save(new EmployeeEntity("Let", "Bogdan", company3));
-//            employeeRepository.save(new EmployeeEntity("Biro", "Attila", company1));
-//            employeeRepository.save(new EmployeeEntity("Veres", "Pishta", company2));
-//            employeeRepository.save(new EmployeeEntity("Sebastian", "Contras", company3));
-//            employeeRepository.save(new EmployeeEntity("Petru", "Avram", company4));
-//            employeeRepository.save(new EmployeeEntity("Dan", "Andrei", company5));
-//
-//        };
+    @Bean
+    public CommandLineRunner databaseSetup(ICompanyRepository companyRepository, IEmployeeRepository employeeRepository) {
+        return args -> {
+            CompanyEntity company1 = new CompanyEntity("Fortech");
+            CompanyEntity company2 = new CompanyEntity("Endava");
+            CompanyEntity company3 = new CompanyEntity("Pitech+");
+            CompanyEntity company4 = new CompanyEntity("3PillarGlob");
+            CompanyEntity company5 = new CompanyEntity("Scitec");
+
+            companyRepository.save(company1);
+            companyRepository.save(company2);
+            companyRepository.save(company3);
+            companyRepository.save(company4);
+            companyRepository.save(company5);
+
+
+            employeeRepository.save(new EmployeeEntity("Mircea", "Pop", company1));
+            employeeRepository.save(new EmployeeEntity("Suciu", "Andrei", company5));
+            employeeRepository.save(new EmployeeEntity("Let", "Bogdan", company3));
+            employeeRepository.save(new EmployeeEntity("Biro", "Attila", company1));
+            employeeRepository.save(new EmployeeEntity("Veres", "Pishta", company2));
+            employeeRepository.save(new EmployeeEntity("Sebastian", "Contras", company3));
+            employeeRepository.save(new EmployeeEntity("Petru", "Avram", company4));
+            employeeRepository.save(new EmployeeEntity("Dan", "Andrei", company5));
+
+        };
+    }
 
 }
