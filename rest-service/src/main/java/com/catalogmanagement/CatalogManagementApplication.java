3a4e776532cbaa66d6fe4ebd69af5ba52e6aabeb
Bogdan Let <bogdangeorgelet@gmail.com>
2017-12-11 18:10:52 +0200

    added config server and have to continue swagger

--- rest-service/src/main/java/com/catalogmanagement/CatalogManagementApplication.java
+++ rest-service/src/main/java/com/catalogmanagement/CatalogManagementApplication.java
@@ -8,11 +8,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CatalogManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogManagementApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Shop").select()
+                .apis(RequestHandlerSelectors.basePackage("com.catalogmanagement"))
+                .paths(any()).build().apiInfo(new ApiInfo("Shop Services",
+                        "Shop Services", "1.0.0", null,
+                        new Contact("Catalog Magement", null, null), null, null));
+
     }
 
     @Bean
