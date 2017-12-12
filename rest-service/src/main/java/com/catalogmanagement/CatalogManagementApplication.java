2870cd40f22381b1554cb75e28c82dcde74be962
Bogdan <bogdangeorgelet@gmail.com>
2017-12-12 19:07:08 +0200

    added eureka

--- rest-service/src/main/java/com/catalogmanagement/CatalogManagementApplication.java
+++ rest-service/src/main/java/com/catalogmanagement/CatalogManagementApplication.java
@@ -7,10 +7,11 @@ import com.catalogmanagement.repository.IEmployeeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CatalogManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogManagementApplication.class, args);
